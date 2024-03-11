43460ad1d06777b416d7f26eaf297bc39ea20b83
RafiaMoazum <rafiamoazum@gmail.com>
2024-03-11 10:26:17 +0500

    first commit

---
+++ src/components/Task.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./Task.css";
+
+const Task = (props) => {
+
+    const[isCompleted,setIsCompleted] = useState(false)
+
+    const handleComplete =()  =>{
+      setIsCompleted(true)
+    }
+
+    const handleIncomplete = () =>{
+      setIsCompleted(false)
+    }
+
+    const handleDelete = () =>{
+      props.delete();
+    }
+
+    const handleCheck = () =>{
+      setIsCompleted(!isCompleted)
+    }
+
+  return (
+    <>
+    <div className="taskDivider">
+      
+      <h2>Task {props.taskNumber+1}</h2>
+      <div className="container">
+      <p className={`description ${isCompleted === true ? "taskDone" : "notDone"}`}>
+            Description: {props.desc}
+      </p>
+
+        {/* <input type="checkbox" onClick={handleCheck}></input> */}
+        <button className="completeBtn" onClick={handleComplete}>Completed</button>
+        <button className="IncompleteBtn" onClick={handleIncomplete}>Incomplete</button>
+        <button className="deleteBtn" onClick={() => props.delete(props.taskNumber)}>Delete</button>
+
+
+      
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default Task;
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import Task from "./Task.jsx";
+import "./TaskList.css"
+
+
+function TaskList() {
+
+  //const taskList=["Node Project","React Project","Softskills Homework","Home Task"]
+
+  const [taskList,setTaskList] =useState([]);
+  const [taskInput,setTaskInput] = useState()
+
+  const handleInput = (e) =>{
+    setTaskInput(e.target.value)
+  }
+
+  const handleAdd = () =>{
+    if(taskInput != '')
+    {
+        setTaskList([...taskList,taskInput]);  //important
+        setTaskInput("");
+    }
+   
+  }
+
+  const handleDelete = (index) => {
+
+    const updatedTaskList = [...taskList];
+    updatedTaskList.splice(index,1);
+
+    setTaskList(updatedTaskList)
+
+    //setTaskList(taskList.splice(index,1))
+  };
+
+  return (
+    <>
+    <div className="main">
+    <h1>To do list</h1>
+    <div className="taskListContainer">
+    <input type="text" placeholder="Enter the Task" value={taskInput} onChange={handleInput}></input>
+    <button style={{margin:"5px", padding:"5px"}} onClick={handleAdd}>Add Task</button>
+    </div>
+    <br></br>
+   
+    <div className="inner">
+    {taskList.map((ele,index) => {
+      return <Task key={index} taskNumber={index} desc={ele} delete={handleDelete} />
+    })}
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default TaskList
+
+
+
+
+
+
+
+
+
